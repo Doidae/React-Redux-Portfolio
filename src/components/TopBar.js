3d5cf005eb7111ca25fc6fc3bebcc49e7df56065
Doidae <andyarredondo1224@gmail.com>
2024-03-07 21:45:20 -0500

    Added bootstrap

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,14 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 export default function TopBar(){
     return (
-        <div>
-            
-        </div>
+        <Navbar expand="lg" className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand href='#home'>Andy C Arredondo</Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
