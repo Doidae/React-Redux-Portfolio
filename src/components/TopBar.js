e1d8b43582ad07757219d02da4c3d99e15ed5a89
Doidae <andyarredondo1224@gmail.com>
2024-03-07 21:20:07 -0500

    Basic TopBar setup

---
+++ src/components/TopBar.js
@@ -0,0 +1,11 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+export default function TopBar(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
